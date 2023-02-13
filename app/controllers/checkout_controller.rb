5d628692db1f8f6e250aff0de7ed49413ce2f11c
IgorFZ <igorf.zamp@gmail.com>
2023-02-13 13:39:09 -0300

    Add Sales Counter

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -9,7 +9,7 @@ class CheckoutController < ApplicationController
                 quantity: 1
             }],
             mode: 'payment',
-            success_url: checkout_success_url,
+            success_url: checkout_success_url + "?session_id={CHECKOUT_SESSION_ID}",
             cancel_url: checkout_cancel_url
         )
         # redirect_to session.url, allow_other_host: true
@@ -20,6 +20,11 @@ class CheckoutController < ApplicationController
     end
     
     def success
+        session_with_expand = Stripe::Checkout::Session.retrieve({ id: params[:session_id], expand: ["line_items"]})
+        session_with_expand.line_items.data.each do |line_item| 
+            product = Product.find_by(stripe_product_id: line_item.price.product)
+            product.increment!(:sales_count)
+        end
     end
     
     def cancel
@@ -28,7 +33,7 @@ class CheckoutController < ApplicationController
      private
     
     def checkout_success_url
-        url_for(action: 'success', only_path: false)
+        url_for(action: 'success', only_path: false, session_id: params[:session_id])
     end
     
     def checkout_cancel_url
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_13_133943) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_13_144431) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_13_133943) do
     t.datetime "updated_at", null: false
     t.string "stripe_product_id"
     t.string "stripe_price_id"
+    t.integer "sales_count", default: 0
   end
 
   create_table "users", force: :cascade do |t|

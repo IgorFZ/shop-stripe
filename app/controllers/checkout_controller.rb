483a7c48933c3bde47bafcb6a992f80a56df33e7
IgorFZ <igorf.zamp@gmail.com>
2023-02-15 16:14:25 -0300

    Allow promotion codes to checkout

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -5,6 +5,7 @@ class CheckoutController < ApplicationController
         @stripe_checkout_session = Stripe::Checkout::Session.create(
             customer: current_user.stripe_customer_id,
             payment_method_types: ['card'],
+            allow_promotion_codes: true,
             line_items: @cart.collect { |item| item.to_builder.attributes! },
             mode: 'payment',
             success_url: checkout_success_url + "?session_id={CHECKOUT_SESSION_ID}",
--- app/models/product.rb
+++ app/models/product.rb
@@ -19,7 +19,7 @@ class Product < ApplicationRecord
 
     after_update :create_and_assign_new_stripe_price, if: :saved_change_to_price?
     def create_and_assign_new_stripe_price
-        price = Stripe::Price.create(product: self.stripe_product_id, unit_amount: self.price, currency: "usd")
+        price = Stripe::Price.create(product: self.stripe_product_id, unit_amount: self.price, currency: "brl")
         update(stripe_price_id: price.id)
     end
 

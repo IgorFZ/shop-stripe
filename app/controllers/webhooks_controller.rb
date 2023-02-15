4982f54aed90787f59c55759fee3c40d57505125
IgorFZ <igorf.zamp@gmail.com>
2023-02-15 08:58:28 -0300

    Testes

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -1,5 +1,5 @@
 class WebhooksController < ApplicationController
-    # skip_before_action :authenticate_user!
+    skip_before_action :authenticate_user!
     skip_before_action :verify_authenticity_token
 
     def create
@@ -8,9 +8,9 @@ class WebhooksController < ApplicationController
         event = nil
 
         begin
-        event = Stripe::Webhook.construct_event(
-            payload, sig_header, Rails.application.credentials[:stripe][:webhook]
-        )
+            event = Stripe::Webhook.construct_event(
+                payload, sig_header, Rails.application.credentials[:stripe][:webhook]
+            )
         rescue JSON::ParserError => e
             status 400
             return
@@ -30,14 +30,14 @@ class WebhooksController < ApplicationController
             puts "SESSION"
             p session.id
             puts "#####################"
-            session_with_expand = Stripe::Checkout::Session.retrieve({ id: session.id, expand: ["line_items"]})
-            session_with_expand.line_items.data.each do |line_item|
-                product = Product.find_by(stripe_product_id: line_item.price.product)
-                puts "#####################"
-                p product
-                puts "#####################"
-                product.increment!(:sales_count)
-            end
+            # session_with_expand = Stripe::Checkout::Session.retrieve({ id: session.id, expand: ["line_items"]})
+            # session_with_expand.line_items.data.each do |line_item|
+            #     product = Product.find_by(stripe_product_id: line_item.price.product)
+            #     puts "#####################"
+            #     p product
+            #     puts "#####################"
+            #     product.increment!(:sales_count)
+            # end
         end
 
         status 200

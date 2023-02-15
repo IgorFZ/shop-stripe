599f0b68a03b2adbf240653a912e87832f354b01
IgorFZ <igorf.zamp@gmail.com>
2023-02-15 10:30:47 -0300

    Exposing Payment Success Information

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -9,7 +9,7 @@ class CheckoutController < ApplicationController
                 quantity: 1
             }],
             mode: 'payment',
-            success_url: checkout_success_url,
+            success_url: checkout_success_url + "?session_id={CHECKOUT_SESSION_ID}",
             cancel_url: checkout_cancel_url
         )
     end
@@ -19,6 +19,10 @@ class CheckoutController < ApplicationController
     end
     
     def success
+        @session_with_expand = Stripe::Checkout::Session.retrieve({id: params[:session_id], expand: ["line_items"]})
+        @session_with_expand.line_items.data.each do |line_item|
+            product = Product.find_by(stripe_product_id: line_item.price.product)
+        end
     end
     
     def cancel
@@ -27,7 +31,7 @@ class CheckoutController < ApplicationController
      private
     
     def checkout_success_url
-        url_for(action: 'success', only_path: false)
+        url_for(action: 'success', only_path: false, session_id: params[:session_id])
     end
     
     def checkout_cancel_url

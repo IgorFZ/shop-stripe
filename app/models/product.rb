282f2b3df4efe16485372a7e3fa595454284d89f
IgorFZ <igorf.zamp@gmail.com>
2023-02-15 16:01:37 -0300

    Updating Pricing At Stripe If Local Price Change

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,13 +8,19 @@ class Product < ApplicationRecord
     
     after_create do
         product = Stripe::Product.create(name: name)
-        price_id = Stripe::Price.create({
+        product_price = Stripe::Price.create({
             unit_amount: price,
             currency: 'brl',
             # recurring: {interval: 'once'},
             product: product.id,
         })
-        update(stripe_price_id: price_id.id, stripe_product_id: product.id)
+        update(stripe_price_id: product_price.id, stripe_product_id: product.id)
+    end
+
+    after_update :create_and_assign_new_stripe_price, if: :saved_change_to_price?
+    def create_and_assign_new_stripe_price
+        price = Stripe::Price.create(product: self.stripe_product_id, unit_amount: self.price, currency: "usd")
+        update(stripe_price_id: price.id)
     end
 
     def to_builder

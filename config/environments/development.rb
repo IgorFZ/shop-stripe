6847fc8d75f83a05ba322b10a2ff3f40b28e095f
IgorFZ <igorf.zamp@gmail.com>
2023-02-15 10:35:00 -0300

    Config Host

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,7 +3,7 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.hosts << "shop-stripe-production-32cb.up.railway.app"
+  config.hosts << "web-production-11ad.up.railway.app"
 
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
